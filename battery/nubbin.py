f66aea40dc3b1f2e86d4f406224762eb1cb784dd
Aylin <aylindemetci@gmail.com>
2023-01-26 15:38:11 -0600

    changes

---
+++ battery/nubbin.py
@@ -0,0 +1,14 @@
+from battery.battery import Battery
+from numpy import timedelta64 as np
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        battery_service_upcoming_date = self.last_service_date + np.timedelta64(4,'Y')
+        if battery_service_upcoming_date < self.current_date:
+            return True
+        else:
+            return False
